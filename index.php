3772d0f51908041b919468b936a3e5d6322c3787
KarthikeyanUTH <124378725+KarthikeyanUTH@users.noreply.github.com>
2023-02-28 10:39:16 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Page Title</title>
+    <title>Page Temptings</title>
   </head>
 
   <body>
     
     <?php
-    echo "<h1>Hello, World!</h1>\n";
+    echo "<h1>Hello, Pmat!</h1>\n";
     ?>
-    
+    </html>
   </body>
-  
+</header>
+</buttons> 
+ { border: none; color: white; padding: 55px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; } .button1 {background-color: #4CAF50;}
 </html>
